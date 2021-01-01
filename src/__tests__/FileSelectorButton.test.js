85ea4d1367d4e8c7d61e9ceec0587c8c5697a0b9
nickpollardAV <40467384+nickpollardAV@users.noreply.github.com>
2021-01-01 20:57:26 +0000

    Create FileSelectorButton.test.js

---
+++ src/__tests__/FileSelectorButton.test.js
@@ -0,0 +1,25 @@
+import React from 'react'
+
+import { shallow } from 'enzyme'
+import FileSelectorButton from '../components/FileSelectorButton'
+
+describe('FileSelectorButton', () => {
+
+    let wrapper
+    beforeEach(() => {
+        wrapper = shallow(<FileSelectorButton handleChange="handleChangeStub"/>)
+    })
+
+    test('renders file uploader`', () => {
+        // console.log(wrapper.debug())
+        expect(wrapper.find('input').props()['type']).toBe('file')
+    })
+
+    test('only accepts images in the uploader', () => {
+        expect(wrapper.find('input').props()['accept']).toBe('image/*')
+    })
+
+    test('allows the user to input an image through a styled button which presses the image-input button', () => {
+        expect(wrapper.find('label').props()['htmlFor']).toBe('image-input')
+    })
+})
