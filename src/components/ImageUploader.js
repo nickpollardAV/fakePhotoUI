9deee94486da55288387eb7b7418cfd95b29ea50
nickpollardAV <40467384+nickpollardAV@users.noreply.github.com>
2021-01-02 20:58:47 +0000

    Update ImageUploader.js

--- src/components/ImageUploader.js
+++ src/components/ImageUploader.js
@@ -1,20 +1,43 @@
-import React, { useState } from 'react'
-import Image from '../images/default-img.jpg'
+import React, { useEffect, useState } from 'react'
+import defaultImage from '../images/default-img.jpg'
 import '../css/ImageUploader.css'
 import FileSelectorButton from './FileSelectorButton'
+import axios from 'axios'
 
 
 function ImageUploader() {
-    const [image, setImage] = useState(Image)
+    const [image, setImage] = useState(defaultImage)
+    const [displayInfo, setDisplayInfo] = useState(false)
 
     const handleChange = (e) => {
         const reader = new FileReader();
         reader.onload = () => {
             if(reader.readyState === 2) {
                 setImage(reader.result)
+                setDisplayInfo(false)
+                console.log(displayInfo)
+                console.log(reader.result)
             }
         }
         reader.readAsDataURL(e.target.files[0])
+    }
+
+    const handleClick = async() => {
+        
+        if (image !== defaultImage) {
+            try {
+                await axios.post('url', 'date')
+                console.log("get successss")
+            } catch(e) {
+                // need to add test for error section
+                // has been added to prevent browswer warnings
+                console.log("error")
+            }
+        } else {
+            setDisplayInfo(true)
+            console.log(displayInfo)
+        }
+        
     }
 
     return (
@@ -23,7 +46,11 @@ function ImageUploader() {
 
             <FileSelectorButton handleChange={handleChange}/>
 
-            {/* <button onClick={fileUploadHandler}>Upload</button> */}
+            <button onClick={handleClick}>Upload</button>
+
+            {displayInfo === false ? null: 
+            <h1 id="information">Please select your own image</h1>}
+            
         </div>
         
     )
