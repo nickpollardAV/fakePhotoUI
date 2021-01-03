e99ac30de42999b84100bf23aad3c03fb7e45854
nickpollardAV <40467384+nickpollardAV@users.noreply.github.com>
2021-01-03 21:37:22 +0000

    Update imageUtils.js

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -1,14 +1,16 @@
 import axios from 'axios'
 
-async function sendImageToServer (image) {
-    try {
-        const formData= new FormData()
-        formData.append(process.env.REACT_APP_BACKEND_URL, image)
-        let response = await axios.post(formData)
-        return response
-    } catch(exception) {
-        throw new Error(exception)
+const imageUtils = {
+    sendImageToServer: async function sendImageToServer (image) {
+        try {
+            const formData= new FormData()
+            formData.append(process.env.REACT_APP_BACKEND_URL, image)
+            let response = await axios.post(formData)
+            return response
+        } catch(exception) {
+            throw new Error(exception)
+        }
     }
 }
 
-exports.sendImageToServer = sendImageToServer
+export default imageUtils
