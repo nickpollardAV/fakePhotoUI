8b0fef27763ebadf3bc217ecb3230a81d35c2223
nickpollardAV <nickpollard23@hotmail.com>
2021-01-16 20:59:41 +0000

    Update post req

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -5,7 +5,8 @@ const imageUtils = {
         try {
             const formData= new FormData()
             formData.append(process.env.REACT_APP_BACKEND_URL, image)
-            let response = await axios.post(formData)
+            let response = await axios.post(process.env.REACT_APP_BACKEND_URL, formData)
+            console.log(response)
             return response
         } catch(exception) {
             throw new Error(exception)
