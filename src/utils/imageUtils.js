5570c8e1fc5ee0d64ac5bf0239c087d9f03ea4a9
nickpollardAV <nickpollard23@hotmail.com>
2021-06-24 08:39:08 +0100

    Test commit

--- src/utils/imageUtils.js
+++ src/utils/imageUtils.js
@@ -4,8 +4,10 @@ const imageUtils = {
     sendImageToServer: async function sendImageToServer (image) {
         try {
             const formData= new FormData()
-            formData.append(process.env.REACT_APP_BACKEND_URL, image)
-            let response = await axios.post(process.env.REACT_APP_BACKEND_URL, formData)
+            formData.append('image', image, "formdataname")
+            console.log(formData)
+            let response = await axios.post(process.env.REACT_APP_BACKEND_URL, {"test": "post"})
+            // let response = await axios.post(process.env.REACT_APP_BACKEND_URL, formData)
             console.log(response)
             return response
         } catch(exception) {
